09499aeaa2782e84d749aa1a92cce95b99de416f
Mateusz <atthew98jura@gmail.com>
2019-05-03 19:44:39 +0200

    added java project vol.2.1

--- Java_Lab02/src/main/java/MainWindow.java
+++ Java_Lab02/src/main/java/MainWindow.java
@@ -3,26 +3,75 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
 import java.util.Random;
+import javax.swing.Timer;
 
 class MainWindow extends JFrame {
     private JPanel panelMain;
     private JButton randomButton;
 
+    static  double x2;
+    static  double y2;
+    static  int flag;
+
     public MainWindow() {
         super("Window");
+
+
+
         panelMain.setLayout(null);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         randomButton.setBounds(screenSize.width/2,screenSize.height/2,150,40);
-        randomButton.addActionListener(new ActionListener() {
+        randomButton.addActionListener(  e -> {
+
+
+            Random generator = new Random();
+
+            double x1 = generator.nextInt(screenSize.width-150);
+            double y1 = generator.nextInt(screenSize.height-40);
+            x2 = randomButton.getX();
+            y2 = randomButton.getY();
+
+            Timer timer = new Timer(10, new ActionListener() {
+
             @Override
+
             public void actionPerformed(ActionEvent e) {
-                Random generator = new Random();
-                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-                int x1 = generator.nextInt(screenSize.width-150);
-                int y1 = generator.nextInt(screenSize.height-40);
-                randomButton.setBounds(x1,y1,150,40);
-            }
+
+                flag++;
+                double x3 = (x1-x2)/100;
+                double y3 = (y1-y2)/100;
+                Point point = new Point();
+                x2 += x3;
+                y2 += y3;
+                point.setLocation(x2, y2);
+                randomButton.setLocation(point);
+                System.out.println(flag);
+
+            }});
+                timer.start();
+
+                Timer timerStop = new Timer(10, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                   if(timer.isRunning()){
+                       if(flag == 100){
+                           timer.stop();
+                           flag = 0;
+                           System.out.println(x1);
+                           System.out.println(x2);
+                       }
+                   }
+                    }
+                });
+                timerStop.start();
         });
+
+       // timer = new Timer(50, new ActionListener() {
+           // @Override
+           // public void actionPerformed(ActionEvent e) {
+
+         //   }
+        //})
 
         panelMain.add(randomButton);
         setContentPane(panelMain);
@@ -32,6 +81,4 @@ class MainWindow extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
-
 }
